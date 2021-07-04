d1b1a66bf94afc24e9b72ec8723dd6f6985a9c1b
Elliot <elliotreborn@gmail.com>
2021-07-04 11:17:24 +0800

    temp

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "img/icon.svg",
   organizationName: "derealize", // Usually your GitHub org/user name.
   projectName: "derealize", // Usually your repo name.
   themeConfig: {
@@ -14,7 +14,7 @@ module.exports = {
       title: "Derealize",
       logo: {
         alt: "Logo",
-        src: "img/logo.svg",
+        src: "img/icon.svg",
       },
       items: [
         {
